682a0b4153f64865e665c3dfecbb3401d060b20c
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-10-01 00:16:41 +0300

    add filter

--- src/higher_order_functions.erl
+++ src/higher_order_functions.erl
@@ -1,5 +1,5 @@
 -module(higher_order_functions).
--export([map/2, reduce/3]).
+-export([map/2, reduce/3, filter/2]).
 
 %% @doc map the given function onto each element of the list
 map(_, []) -> [];
@@ -8,3 +8,12 @@ map(F, [H|T]) -> [F(H)|map(F, T)].
 %% @doc reduce the elements of the list using given function and accumulator value
 reduce(_, Acc, []) -> Acc;
 reduce(F, Acc, [H|T]) -> reduce(F, F(Acc, H), T).
+
+%% @doc return items for which predicate is true
+filter(Pred, L) -> filter(Pred, L, []).
+filter(_, [], Acc) -> lists:reverse(Acc);
+filter(Pred, [H|T], Acc) ->
+  case Pred(H) of
+    true -> filter(Pred, T, [H|Acc]);
+    false -> filter(Pred, T, Acc)
+  end.
