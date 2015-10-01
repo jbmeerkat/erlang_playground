24b827d7668c32d783cdae355d8202759fab1f2f
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-10-02 02:40:14 +0300

    play with list comprehensions

--- src/higher_order_functions.erl
+++ src/higher_order_functions.erl
@@ -1,9 +1,12 @@
 -module(higher_order_functions).
--export([map/2, reduce/3, filter/2]).
+-export([map/2, lc_map/2, reduce/3, filter/2, lc_filter/2]).
 
 %% @doc map the given function onto each element of the list
 map(_, []) -> [];
 map(F, [H|T]) -> [F(H)|map(F, T)].
+
+%% @doc same as map, but implemented using list comprehensions
+lc_map(F, L) -> [ F(I) || I <- L].
 
 %% @doc reduce the elements of the list using given function and accumulator value
 reduce(_, Acc, []) -> Acc;
@@ -17,3 +20,6 @@ filter(Pred, [H|T], Acc) ->
     true -> filter(Pred, T, [H|Acc]);
     false -> filter(Pred, T, Acc)
   end.
+
+%% @doc same as filter, but implemented using list comprehensions
+lc_filter(Pred, L) -> [X || X <- L, Pred(X)].
